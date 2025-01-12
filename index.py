26562569a015e8992c2f5d752795885554b733da
YUGESHKARAN <yugeshkaran01@gmail.com>
2025-01-12 22:30:54 +0530

    update-3 backend created

--- index.py
+++ index.py
@@ -80,4 +80,5 @@ def handle_request():
 
 
 if __name__ == '__main__':
-  app.run(port=3000,debug=False)
+  port = int(os.environ.get("PORT", 3000))  # Default to 3000 if PORT is not set
+  app.run(host="0.0.0.0", port=port, debug=False)
